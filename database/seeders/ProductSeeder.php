8f867720001ab5c8ced71de8dd54b8661452d3df
Tamás Major <majortamas313@gmail.com>
2023-03-02 00:07:55 +0100

    static data product seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = new \App\Models\Product([
+            'name' => 'Product #1',
+            'detail' => 'Product #1 details'
+        ]);
+        $product->save();
+
+        $product = new \App\Models\Product([
+            'name' => 'Product #2',
+            'detail' => 'Product #2 details'
+        ]);
+        $product->save();
+
+        $product = new \App\Models\Product([
+            'name' => 'Product #3',
+            'detail' => 'Product #3 details'
+        ]);
+        $product->save();
+    }
+}
